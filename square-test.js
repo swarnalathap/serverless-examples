763e63042f136aac9c75e5c6213225e79ec3fc04
Swarnalatha Pasupuleti <45089087+swarnalathap@users.noreply.github.com>
2019-02-13 13:48:56 +0530

    Update square-test.js

--- square-test.js
+++ square-test.js
@@ -1,8 +1,11 @@
 module.exports = x => {
     const xx = x ** 2;
     console.log(`the square of ${x} is ${xx}`);
-    i = 0;
-    while (i < 1000) {
+    i = 0; j = 0;
+    while (i < 100000) {
+        while (j < 10000) {
+           j++;
+        }
         i++;
     }
     return xx;

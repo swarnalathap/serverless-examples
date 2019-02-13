0c26bc0e31f80be725939fffeb2093b378e4d3a4
Swarnalatha Pasupuleti <45089087+swarnalathap@users.noreply.github.com>
2019-02-13 13:38:37 +0530

    Update square-test.js

--- square-test.js
+++ square-test.js
@@ -1,7 +1,9 @@
 module.exports = x => {
     const xx = x ** 2;
     console.log(`the square of ${x} is ${xx}`);
-    var sleep = require('sleep');
-    sleep.sleep(100);
+    i = 0;
+    while (i < 1000) {
+        i++;
+    }
     return xx;
 }
